1e3a90191b2e585097b1f4f15a27aaab089bf178
Ash_zd <ash_zd@qq.com>
2018-07-10 16:01:47 +0800

    rails

--- rails/sample_app/config/routes.rb
+++ rails/sample_app/config/routes.rb
@@ -1,9 +1,16 @@
 Rails.application.routes.draw do
-  get 'users/new'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'static_pages#home'
   get '/help', to: 'static_pages#help'
   get '/about', to: 'static_pages#about'
   get '/contact', to: 'static_pages#contact'
+
   get '/signup', to: 'users#new'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'static_pages#home'
+  post '/signup', to: 'users#create'
+
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+
+  resources :users
 end
