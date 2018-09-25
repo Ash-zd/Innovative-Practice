b17ae09ad9c0b1e914b008b6da2a5561f9923156
Ash_zd <ash_zd@qq.com>
2018-09-25 10:38:59 +0800

    update readme

--- rails/toy_app/app/models/micropost.rb
+++
@@ -1,4 +0,0 @@
-class Micropost < ApplicationRecord
-  validates :content, length: { maximum: 140 }, presence: true
-  belongs_to :user
-end
--- rails/toy_app/app/models/user.rb
+++
@@ -1,5 +0,0 @@
-class User < ApplicationRecord
-  has_many :microposts
-  #validate FILL_IN, presence: true
-
-end
